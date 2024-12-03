63b03c1de9ed877527be7433e80c30672d4ade32
Daniel Trifonov <79804297+danito7718@users.noreply.github.com>
2024-12-03 12:32:54 +0200

    Created a Weapon Function in C++
    Created a Weapon Function in C++, and Inherited the functions from Item

--- Source/MyProject2/Private/Items/Item.cpp
+++ Source/MyProject2/Private/Items/Item.cpp
@@ -39,6 +39,7 @@ float AItem::TransformedCos()
 	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
+
 void AItem::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	const FString OtherActorName = OtherActor->GetName();
